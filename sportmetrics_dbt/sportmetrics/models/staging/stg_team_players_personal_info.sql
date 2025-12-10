2ba84e602251782080c01ad26c30000619f3e592
liokal57 <liokal57@gmail.com>
2025-12-10 09:15:37 +0100

    Delete sportmetrics_dbt directory

--- sportmetrics_dbt/sportmetrics/models/staging/stg_team_players_personal_info.sql
+++
@@ -1,2 +0,0 @@
-{{ config(materialized='view') }}
-select * from {{ source('sportmetrics_raw', 'team_players_personal_info') }}
--- sportmetrics_dbt/sportmetrics/models/staging/stg_team_training_sessions.sql
+++
@@ -1,2 +0,0 @@
-{{ config(materialized='view') }}
-select * from {{ source('sportmetrics_raw', 'team_training_sessions') }}
